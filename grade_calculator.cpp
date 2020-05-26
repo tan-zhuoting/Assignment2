c0c35fa81b5fda0e123a96d6b28974aa8dbd88b3
Zhuoting Tan <zhuoting@ucla.edu>
2020-05-26 02:05:42 -0700

    add members to course_data to store score info

--- grade_calculator.cpp
+++ grade_calculator.cpp
@@ -1,3 +1,4 @@
+#include <QString>
 #include "grade_calculator.h"
 #include "ui_grade_calculator.h"
 
@@ -6,6 +7,7 @@ grade_calculator::grade_calculator(QWidget *parent)
     , ui(new Ui::grade_calculator)
 {
     ui->setupUi(this);
+    course_datas.resize(3);
 }
 
 grade_calculator::~grade_calculator()
@@ -13,3 +15,35 @@ grade_calculator::~grade_calculator()
     delete ui;
 }
 
+
+void grade_calculator::on_course_comboBox_activated(int index)
+{
+   ui->horizontalSlider->setValue(course_datas[index].hw1);
+   ui->spinBox->setValue(course_datas[index].hw1);
+   ui->horizontalSlider_2->setValue(course_datas[index].hw2);
+   ui->spinBox_2->setValue(course_datas[index].hw2);
+   ui->horizontalSlider_3->setValue(course_datas[index].hw3);
+   ui->spinBox_3->setValue(course_datas[index].hw3);
+   ui->horizontalSlider_4->setValue(course_datas[index].hw4);
+   ui->spinBox_4->setValue(course_datas[index].hw4);
+   ui->horizontalSlider_5->setValue(course_datas[index].hw5);
+   ui->spinBox_5->setValue(course_datas[index].hw5);
+   ui->horizontalSlider_6->setValue(course_datas[index].hw6);
+   ui->spinBox_6->setValue(course_datas[index].hw6);
+   ui->horizontalSlider_7->setValue(course_datas[index].hw7);
+   ui->spinBox_7->setValue(course_datas[index].hw7);
+   ui->horizontalSlider_8->setValue(course_datas[index].hw8);
+   ui->spinBox_8->setValue(course_datas[index].hw8);
+   ui->horizontalSlider_9->setValue(course_datas[index].midterm1);
+   ui->spinBox_9->setValue(course_datas[index].midterm1);
+   ui->horizontalSlider_10->setValue(course_datas[index].midterm2);
+   ui->spinBox_10->setValue(course_datas[index].midterm2);
+   ui->horizontalSlider_11->setValue(course_datas[index].finalexam);
+   ui->spinBox_11->setValue(course_datas[index].finalexam);
+   if (course_datas[index].schema == 1) {
+       ui->radioButton->toggle();
+   } else {
+       ui->radioButton_2->toggle();
+   }
+   ui->label_14->setText(QString::number(course_datas[index].overall, 'g', 3));
+}
--- grade_calculator.h
+++ grade_calculator.h
@@ -2,10 +2,28 @@
 #define GRADE_CALCULATOR_H
 
 #include <QMainWindow>
+#include <QVector>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class grade_calculator; }
 QT_END_NAMESPACE
+
+struct CourseData
+{
+    int hw1 = 0;
+    int hw2 = 0;
+    int hw3 = 0;
+    int hw4 = 0;
+    int hw5 = 0;
+    int hw6 = 0;
+    int hw7 = 0;
+    int hw8 = 0;
+    int midterm1 = 0;
+    int midterm2 = 0;
+    int finalexam = 0;
+    int schema = 1;
+    double overall = 0.0;
+};
 
 class grade_calculator : public QMainWindow
 {
@@ -15,7 +33,12 @@ public:
     grade_calculator(QWidget *parent = nullptr);
     ~grade_calculator();
 
+private slots:
+    void on_course_comboBox_activated(int index);
+
 private:
     Ui::grade_calculator *ui;
+
+    QVector<CourseData> course_datas;
 };
 #endif // GRADE_CALCULATOR_H
